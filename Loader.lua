be7296fc212f729406cac482cbda6f131d447793
Jack <113581836+velayius@users.noreply.github.com>
2023-04-27 12:58:01 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -12,4 +12,4 @@ local Place = Places[game.PlaceId]
 if not Place or not Place["Updated"] then return end
 local Name = Place["Name"]
 
-loadstring(game:HttpGet(string.format("%s/Games/%s.lua", Repository, Name)))()
+loadstring(game:HttpGet(string.format("%s/Games/%s.lua", Repository, Place["Name"])))()
