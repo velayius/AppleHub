34918cd949a4913d1105670dd9860d701119e3e0
Jack <113581836+velayius@users.noreply.github.com>
2023-04-27 13:10:41 +0100

    lol

--- Games/Baseplate.lua
+++ Games/Baseplate.lua
@@ -1,1 +1,1 @@
-print('test')
+print(game.PlaceId)
--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,3 @@
-local Repository = "https://github.com/velayius/AppleHub"
-
 local Places = {
     [13201222072] = {
         ["Name"] = "Baseplate",
@@ -12,4 +10,8 @@ local Place = Places[game.PlaceId]
 if not Place or not Place["Updated"] then return end
 local Name = Place["Name"]
 
-loadstring(game:HttpGet(string.format("%s/Games/%s.lua", Repository, Place["Name"])))()
+if Place then
+    if Places[13201222072] then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/velayius/AppleHub/main/Games/Baseplate.lua"), true)()
+    end
+end
