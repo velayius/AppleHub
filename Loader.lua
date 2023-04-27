02705451c4b9c7d694573a41f028cc78d343b339
Jack <113581836+velayius@users.noreply.github.com>
2023-04-27 12:53:15 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,15 @@
-test
+local Repository = "https://github.com/velayius/AppleHub"
+
+local Places = {
+    [13201222072] = {
+        ["Name"] = "Baseplate",
+        ["Updated"] = true,
+    },
+}
+
+local Place = Places[game.PlaceId]
+
+if not Place or not Place["Updated"] then return end
+local Name = Place["Name"]
+
+loadstring(game:HttpGet(string.format("%s/Games/%s.lua", Repository, Name)))()
